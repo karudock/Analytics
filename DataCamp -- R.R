aae61faf1de14f82fdd7ba5af63abb9733013287
karudock <52300179+karudock@users.noreply.github.com>
2020-02-19 12:01:01 -0500

    Add files via upload

---
+++ DataCamp -- R.R
@@ -0,0 +1,54 @@
+
+####################
+# Creating Vectors #
+###################
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign the names of the day to roulette_vector and poker_vector
+# AKA add a new column to create a matrix
+names(poker_vector) <-   days_vector
+names(roulette_vector) <- days_vector
+
+
+######################
+# Calculating Totals #
+######################
+
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <- A_vector + B_vector
+
+# Print out total_vector
+total_vector
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Total winnings with poker
+total_poker <- sum(poker_vector)
+
+# Total winnings with roulette
+total_roulette <-  sum(roulette_vector)
+
+# Total winnings overall
+total_week <- sum(total_roulette, total_poker)
+
+# Print out total_week
+total_week
+
+
+
